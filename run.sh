19c99b0e37cd5165250fd1ad225ba0b28b05217b
Daniel Stenberg <daniel@haxx.se>
2026-08-11 22:39:29 +0200

    run: clear the out/* before scan runs
    We now make random file names

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ CODE=$HOME/src/curl-perf-code
 echo "runs single.sh $CODE to $LOG"
 ./single.sh $CODE $PERFDIR > log/$LOG
 echo "now make the HTML"
+rm -f out/*
 ./scan.pl > out/index.html
 ./tarballit.sh out perf.tar.gz
