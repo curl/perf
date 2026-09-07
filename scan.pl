19daf7485b44955c65fc0a0336c89b2bc87e1b48
Daniel Stenberg <daniel@haxx.se>
2026-09-07 19:43:47 +0200

    scan: make the moving average the 25 last rounds

--- scan.pl
+++ scan.pl
@@ -16,7 +16,7 @@ my $roundspergraph = 100;
 my $deltathreshold = 0.7;
 
 # Use these many values for the moving average
-my $movingaverage = 10;
+my $movingaverage = 25;
 
 # number of entries to store in the "full range" LTTB CSV file
 my $numlttb = 100;
